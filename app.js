6f2abca6fa805d8982e7df43a15fd6e1dfc173f1
Muthoni Kevin Gatamu <49341231+KevGMR@users.noreply.github.com>
2022-05-06 09:27:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,35 @@ require('dotenv').config();
 
 
 const express = require('express');
+const app = express();
+
+const notFoundMiddleware  = require('./middleware/not-found')
+const errorMiddleware  = require('./middleware/error-handler')
+
+//middleware
+app.use(express.json())
+
+//routes
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products"> Products Route </a>')
+})
+
+//products route
+
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try{
+        //connectDB
+        app.listen(port, console.log(`Server is litening on Port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
